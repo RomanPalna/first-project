20265d1c09ef4231a540d9004eadcde5837e07bb
Roman Palna <roma.palna@gmail.com>
2020-07-21 22:21:44 +0300

    прпр

--- test.js
+++ test.js
@@ -1,13 +1,56 @@
-let car = {
-  color: 'green',
-  maxSpeed: 250,
-  audio: {
-    brand: 'Sony',
-    speakers: 12,
-  },
-};
+// const age = 33;
+// const userName = 'Roman';
+// console.log('Имя', userName);
+// console.log(typeof age);
 
-car.color = 'red';
+// Приклад Confirm
+// const shouldRenew = confirm('Продлить?');
+// console.log(shouldRenew);
 
-console.log(typeof car.audio);
-console.log(car.audio);
+// Приклад Prompt
+// let quantity = prompt('Скоко денег даш?');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// Числові літерали Number.parseInt
+// let elementWidth = '50px';
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
+
+// Числові літерали Number.parseFloat
+// let elementWidth = '50.544px';
+// const result = Number.parseFloat(elementWidth);
+// console.log(result);
+
+// const salery = 9.2352;
+// salery = Number(salery.toFixed(2));
+// console.log(salery);
+
+// console.log(Number('8989'));
+
+// const base = 3;
+// const power = 5;
+// const result = Math.pow(base, power);
+
+// console.log(result);
+
+// console.log(3 ** 12);
+
+// let base = prompt('Give me a number');
+// base = Number(base);
+// console.log(base);
+
+// let power = prompt('give me a second number');
+// power = Number(power);
+// console.log(power);
+
+// console.log(base ** power);
+
+//Генерация чисел випадкова
+// const max = 100;
+// const min = 1;
+// console.log(Math.round(Math.random() * (max - min) + min));
+
+// const massage = 'тут сколько символов?';
+// console.log(massage.length);
