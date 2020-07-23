8bd19e834187f487965b7297a989cbaf6e2d9788
Roman Palna <roma.palna@gmail.com>
2020-07-23 21:03:26 +0300

    Update test.js

--- test.js
+++ test.js
@@ -54,3 +54,154 @@
 
 // const massage = 'тут сколько символов?';
 // console.log(massage.length);
+
+// SWITCH Задача 1
+// const stars = 4;
+// let price;
+
+// if (stars === 1) {
+//   price = 20;
+// } else if (stars === 2) {
+//   price = 30;
+// } else if (stars === 3) {
+//   price = 50;
+// } else if (stars === 4) {
+//   price = 70;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log('Такого нет');
+// }
+
+// SWITCH
+// switch (stars) {
+//   case 1:
+//     price = 20;
+//     break;
+
+//   case 2:
+//     price = 30;
+//     break;
+
+//   case 3:
+//     price = 50;
+//     break;
+
+//   case 4:
+//     price = 70;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log('Такого нет');
+// }
+
+// console.log(price);
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = 20;
+//     break;
+
+//   case 3:
+//   case 4:
+//     price = 30;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log('Такого нет');
+// }
+
+// console.log(price);
+
+//Задача 2 Switch
+
+// const option = 2;
+// let message = '';
+
+// switch (option) {
+//   case 1:
+//     message = 'Заберите завтра после 12.00';
+//     break;
+//   case 2:
+//     message = 'Доставка курьера';
+//     break;
+//   case 3:
+//     message = 'Посилка будет отправлена';
+//     break;
+//   default:
+//     message = 'Вам перезвонит менеджер';
+//     break;
+// }
+
+// console.log(message);
+
+// Цикл FOR
+// for (let i = 0; i < 5; i += 1) {
+//   console.log(i);
+// }
+
+// Задача 3
+// const minSallery = 500;
+// const maxSellery = 5000;
+// const employees = 3;
+// let totalSallery = 0;
+
+// for (let i = 0; i <= employees; i += 1) {
+//   const salery = Math.round(
+//     Math.random() * (maxSellery - minSallery) + minSallery,
+//   );
+//   console.log(`ЗП работника номер ${i} - ${salery}`);
+
+//   totalSallery += salery;
+// }
+// console.log('Total:', totalSallery);
+
+//Задача 4
+// const min = 6;
+// const max = 13;
+// let total = 0;
+
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 2 !== 0) {
+//     console.log('NOT Четное ', i);
+//     continue;
+//   }
+
+//   console.log('Четное ', i);
+//   total += i;
+// }
+
+// console.log('Тотал: ', total);
+
+let totalSpent = 50;
+const payment = 500;
+let discount = 0;
+
+if (totalSpent < 100) {
+  discount = 0;
+} else if (totalSpent > -100 && totalSpent < 1000) {
+  console.log('Бронза, скидка 2%');
+  discount = 0.02;
+} else if (totalSpent >= 1000 && totalSpent < 5000) {
+  console.log('Серебро, скидка 5%');
+  discount = 0.05;
+} else {
+  console.log('Золото, скидка 5%');
+}
+
+console.log(`Оформляю заказ на суму ${payment} со скидкой ${discount * 100}%`);
+
+payment = payment - payment * discount;
+
+totalSpent += payment;
+
+console.log(`Загальна сума витраченого в магазині: ${totalSpent}`);
